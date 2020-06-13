df83ec48a3758f0d0bffb54350be3b296a4bfa4e
Nelson <nelsonespindolalves@gmail.com>
2020-06-12 21:18:56 -0300

    Add Procfile to Heroku

--- database.js
+++ database.js
@@ -6,7 +6,7 @@ mongoose.Promise = global.Promise;
 //mongoose.connect(`mongodb://${process.env.MONGO_DB_HOST}:${process.env.MONGO_DB_PORT}/${process.env.MONGO_DB_DATABASE}`, {useNewUrlParser: true});
 
 /* Remote access */
-mongoose.connect(`mongodb+srv://elnixenergy:${process.env.MONGO_DB_PASSWORD}@dbservergestao-rwgoh.gcp.mongodb.net/${MONGO_DB_DATABASE}?retryWrites=true&w=majority`, {useNewUrlParser: true});
+mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true});
 
 //mongodb+srv://elnixenergy:<password>@dbservergestao-rwgoh.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority
 
